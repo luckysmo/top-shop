5e01ea5645d8eaa5352ba72f7ac2a5698ba15a1b
aleksey.oshurkov <luckysmo@yandex.ru>
2023-06-07 11:06:52 +0400

    init commit

---
+++ src/main/java/ru/oshurkov/topshop/TopShopApplication.java
@@ -0,0 +1,13 @@
+package ru.oshurkov.topshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/oshurkov/topshop/TopShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.oshurkov.topshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
